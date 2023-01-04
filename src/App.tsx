8af382072479a80bd6481975715e1c502899fd39
Pabllot07 <pabllot07@outlook.com>
2023-01-04 15:53:39 -0300

    routing done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css"
-import { Routes, Route } from 'react-router-dom'
+import { Container } from "react-bootstrap"
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<h1>Hoccups Poccus</h1>} />
-    </Routes>
+    <Container className="my-4">
+      <Routes>
+        <Route path='/' element={<h1>Hoccups Poccus</h1>} />
+        <Route path='/new' element={<h1>New</h1>} />
+        <Route path="/:id">
+          <Route index element={<h1>show id</h1>} />        
+          <Route path="edit" element={<h1>edit</h1>} />        
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
   )
 }
 
