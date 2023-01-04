08c09a8d4fc26c4352b3081b3fdf323927d26bf7
Pabllot07 <pabllot07@outlook.com>
2023-01-04 17:30:04 -0300

    tags

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,8 +1,8 @@
-import { FormEvent, useRef } from 'react'
+import { FormEvent, useRef, useState } from 'react'
 import { Form, Stack, Row, Col, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import CreatableReactSelect from 'react-select/creatable'
-import { NoteData } from './App'
+import { NoteData, Tag } from './App'
 
 type NoteFormProps = {
     onSubmit: (data: NoteData) => void
@@ -11,6 +11,7 @@ type NoteFormProps = {
 const NoteForm = ({onSubmit}: NoteFormProps) => {
     const titleRef = useRef<HTMLInputElement>(null)
     const markdownRef = useRef<HTMLTextAreaElement>(null)
+    const [selectedTags, setSelectedTags] = useState<Tag[]>([])
 
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault()
@@ -35,7 +36,19 @@ const NoteForm = ({onSubmit}: NoteFormProps) => {
                 <Col>
                     <Form.Group controlId='title'>
                         <Form.Label>Tags</Form.Label>
-                        <CreatableReactSelect isMulti />
+                        <CreatableReactSelect
+                            value={selectedTags.map(tag => {
+                                return {label: tag.label, value: tag.id}
+                            })}
+                            onChange={tags => {
+                                setSelectedTags(
+                                    tags.map(tag => {
+                                        return {label: tag.label, id: tag.value}
+                                    })
+                                )
+                            }}
+                            isMulti 
+                        />
                     </Form.Group>
                 </Col>
             </Row>
