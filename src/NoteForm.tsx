c7050568a07f7d4638bc0a9da6f1a17440bd82c0
Pabllot07 <pabllot07@outlook.com>
2023-01-04 17:18:00 -0300

    onSubmit typed

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,16 +1,35 @@
+import { FormEvent, useRef } from 'react'
 import { Form, Stack, Row, Col, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import CreatableReactSelect from 'react-select/creatable'
+import { NoteData } from './App'
 
-const NoteForm = () => {
+type NoteFormProps = {
+    onSubmit: (data: NoteData) => void
+}
+
+const NoteForm = ({onSubmit}: NoteFormProps) => {
+    const titleRef = useRef<HTMLInputElement>(null)
+    const markdownRef = useRef<HTMLTextAreaElement>(null)
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault()
+
+        onSubmit({
+            title: titleRef.current!.value,
+            markdown: markdownRef.current!.value,
+            tags: []
+        })
+    }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
         <Stack gap={4}>
             <Row>
                 <Col>
                     <Form.Group controlId='title'>
                         <Form.Label>Title</Form.Label>
-                        <Form.Control required />
+                        <Form.Control ref={titleRef} required />
                     </Form.Group>
                 </Col>
                 <Col>
@@ -22,7 +41,7 @@ const NoteForm = () => {
             </Row>
             <Form.Group controlId='markDown'>
                 <Form.Label>Body</Form.Label>
-                <Form.Control required as='textarea' rows={15}/>
+                <Form.Control required as='textarea' ref={markdownRef} rows={15}/>
             </Form.Group>
             <Stack direction='horizontal' gap={2} className='justify-content-end'>
                 <Button type='submit' variant='primary'>Save</Button>
